e26c83a39746ad73eb3d36ec3305fda9431899d0
Yakup <yakupjoraev@gmail.com>
2023-08-15 00:58:59 +0300

    added new sections

--- front/build/js/scripts.js
+++ front/build/js/scripts.js
@@ -52,5 +52,67 @@ function fixedNav() {
     nav.classList.remove('fixed__nav')
   }
 }
-window.addEventListener('scroll', fixedNav)
+window.addEventListener('scroll', fixedNav);
+
+
+function tokenomicsSelect() {
+  const container = document.querySelector('.tokenomics');
+
+  if (!container) {
+    return null
+  }
+
+  let tokenomicsItems = document.querySelectorAll('[data-tokenomics-item]');
+
+  const removeClass = () => {
+    tokenomicsItems.forEach(tokenomicsItem => {
+      tokenomicsItem.classList.remove('active')
+    })
+  }
+
+  tokenomicsItems.forEach(tokenomicsItem => {
+
+    tokenomicsItem.addEventListener('click', () => {
+      removeClass();
+      tokenomicsItem.classList.add('active')
+    })
+  });
+}
+
+tokenomicsSelect();
+
+// Аккордеон
+const accordionItems = document.querySelectorAll('[data-accordion-item]');
+let openAccordion = null; // переменная для хранения ссылки на открытый аккордеон
+
+function toggleAccordion() {
+  if (openAccordion && openAccordion !== this) {
+    // Если есть открытый аккордеон, который не совпадает с текущим
+    openAccordion.classList.remove('active'); // закрыть его
+    const openAccordionContent = openAccordion.nextElementSibling;
+    if (openAccordionContent) {
+      // если у аккордеона есть содержимое
+      openAccordionContent.style.maxHeight = null; // сбросить высоту контента
+    }
+  }
+
+  this.classList.toggle('active'); // открыть или закрыть текущий аккордеон
+
+  const content = this.nextElementSibling;
+  if (content) {
+    // если у аккордеона есть содержимое
+    if (content.style.maxHeight) {
+      // Если контент открыт, закрыть его
+      content.style.maxHeight = null;
+    } else {
+      // Если контент закрыт, открыть его
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  }
+
+  openAccordion = this; // запомнить ссылку на открытый аккордеон
+}
+
+
+accordionItems.forEach(item => item.addEventListener('click', toggleAccordion));
 
